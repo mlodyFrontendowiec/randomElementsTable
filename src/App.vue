7a85baa519041517ca7d81b56c7c5215d06b0c29
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2021-11-10 20:47:04 +0100

    Add array of movies and method shuffleArray

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
-<template><p>ok</p></template>
+<template>ok</template>
 
 <script>
 export default {
   name: "App",
+
+  data() {
+    return {
+      movies: [
+        { id: 1, title: "Star Wars" },
+        { id: 2, title: "Indiana Jones" },
+        { id: 3, title: "Jurrasic World" },
+        { id: 4, title: "Skyfall" },
+        { id: 5, title: "The Wolf of Wall Street" },
+        { id: 6, title: "Deadpool" },
+      ],
+    };
+  },
+  methods: {
+    shuffleArray() {
+      this.movies = this.movies.sort(() => Math.random() - 0.5);
+    },
+  },
 };
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
