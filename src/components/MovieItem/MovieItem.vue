281c8f49ad0531f27ba31e2dfde1945f527fe5d5
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2021-11-10 21:43:00 +0100

    Add cursor pointer style

--- src/components/MovieItem/MovieItem.vue
+++ src/components/MovieItem/MovieItem.vue
@@ -19,6 +19,7 @@ div {
   margin-bottom: 15px;
   box-shadow: rgba(0, 0, 0, 0.16) 0px 0.5px 4px, rgb(0, 0, 0) 0px 0px 0px 2px;
   background-color: rgb(103, 188, 190);
+  cursor: pointer;
 }
 p {
   color: white;
